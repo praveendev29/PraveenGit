522349d8649c83bc17f5fed3a61b3005f39ad338
praveendev29 <praveenprofessional29@gmail.com>
2026-05-31 16:29:27 +0530

    Tester1 commit message

--- src/testNGTutorial/firstTest.java
+++ src/testNGTutorial/firstTest.java
@@ -1,24 +1,27 @@
 package testNGTutorial;
 
 public class firstTest {
+	int a = 5;
+	int b = 6;
 
-	
 	public void getFirstTest() {
 
 		System.out.println("hi");
 	}
 
-private void mySecondTest() {
-	System.out.println("this is tester 2");
+	private void mySecondTest() {
+		System.out.println("this is tester 2");
+		System.out.println(a);
 
-}	
-	
-	private void getThird() {
-System.out.println("kumar is sleeping");
 	}
-	
+
+	private void getThird() {
+		System.out.println("kumar is sleeping");
+		System.out.println(b);
+	}
+
 	public static void main(String[] args) {
-		firstTest fs= new firstTest();
+		firstTest fs = new firstTest();
 		fs.getFirstTest();
 		fs.getThird();
 		fs.mySecondTest();
